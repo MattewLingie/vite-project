9124d269b1a05e17d4cbfd7a2c9876d134b73762
Mattew <20FTT1441@student.pb.edu.bn>
2022-04-15 23:37:19 +0800

    vuejs and tailwindcss install via vite

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1 class="font-bold ...">Mattew Lingie Anak Minggat</h1><br>
+    <img src="./assets/my_image.jpeg" alt="No image available">
+    
+    <div id="info">
+      <p>20FTT1441</p>
+      <p>D.O.B: 06/01/2000</p><br>
+      <p>School: School of Information and Technology</p><br>
+      <p>Institution: Politeknik Brunei</p><br>
+      <p>Course: Diploma in Web Development</p>
+    </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
